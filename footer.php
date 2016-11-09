f6e8f63d5e84bac01820e3eac30f1eb371735a5c
Steven Moxley <stevenjmoxley@gmail.com>
2016-11-09 17:27:34 -0500

    Newest additions
    Here is the start files for our project

---
+++ footer.php
@@ -0,0 +1,4 @@
+<div class="footer">
+  <footer>&copy; All Rights Reserved, 6ixtape.</footer>
+      <p>Created by <a href="https://github.com/StevenMox" target="_blank">Steven Moxley</a>, Harsha Sharma , Jabri Haughton, Rajat Chawla </p>
+</div>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+
+    <title>6ixspots</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+  <body>
+    <?php include 'header.php'; ?>
+
+
+    <?php include 'footer.php'; ?>
+  </body>
+</html>
